b28c5e79817fbf2e331c118a56844d18612b2770
shahraiz-cb <shahraiz.ali@crowdbotics.com>
2023-04-20 23:35:16 +0500

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/Untitled1/index.js
+++ screens/Untitled1/index.js
@@ -1,20 +1,38 @@
-import React from "react";
+import { useSelector } from "react-redux";
+import { Text } from "react-native";
+import { bestbuyapi_get_v1_products_list } from "../../store/bestBuyAPI/bestbuyapi_response_get_GetProductsLists.slice.js";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { StyleSheet, ScrollView, SafeAreaView } from "react-native";
 
 const Untitled1 = () => {
+  const {
+    entities: Bestbuyapi_response_get_GetProductsLists
+  } = useSelector(state => state.Bestbuyapi_response_get_GetProductsLists);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(bestbuyapi_get_v1_products_list());
+  }, []);
   return <SafeAreaView style={styles.safeArea}>
         <ScrollView contentContainerStyle={{
       backgroundColor: '#f0f0f1',
       padding: 10,
       position: 'relative',
       flex: 1
-    }}></ScrollView>
+    }}><Text style={styles.IqlmNOOk}>{Bestbuyapi_response_get_GetProductsLists[0].products}</Text></ScrollView>
     </SafeAreaView>;
 };
 
 const styles = StyleSheet.create({
   safeArea: {
     height: '100%'
+  },
+  IqlmNOOk: {
+    width: 100,
+    height: 50,
+    lineHeight: 14,
+    fontSize: 14,
+    borderRadius: 0
   }
 });
 export default Untitled1;
--- store/bestBuyAPI/bestbuyapi_response_get_GetProductWarranties.slice.js
+++ store/bestBuyAPI/bestbuyapi_response_get_GetProductWarranties.slice.js
@@ -1,49 +1,41 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit"
-import { apiService } from "./api"
-export const bestbuyapi_get_v1_products_pid_warrantiesjson_list = createAsyncThunk(
-  "bestbuyapi_response_get_GetProductWarranties/bestbuyapi_get_v1_products_pid_warrantiesjson_list",
-  async payload => {
-    const response = await apiService.bestbuyapi_get_v1_products_pid_warrantiesjson_list(
-      payload
-    )
-    return response.data
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { apiService } from "./api";
+export const bestbuyapi_get_v1_products_pid_warrantiesjson_list = createAsyncThunk("bestbuyapi_response_get_GetProductWarranties/bestbuyapi_get_v1_products_pid_warrantiesjson_list", async payload => {
+  const response = await apiService.bestbuyapi_get_v1_products_pid_warrantiesjson_list(payload);
+  return response.data;
+});
+const initialState = {
+  entities: [],
+  api: {
+    loading: "idle",
+    error: null
   }
-)
-const initialState = { entities: [], api: { loading: "idle", error: null } }
+};
 const bestbuyapi_response_get_GetProductWarrantiesSlice = createSlice({
   name: "bestbuyapi_response_get_GetProductWarranties",
   initialState,
   reducers: {},
   extraReducers: {
-    [bestbuyapi_get_v1_products_pid_warrantiesjson_list.pending]: (
-      state,
-      action
-    ) => {
+    [bestbuyapi_get_v1_products_pid_warrantiesjson_list.pending]: (state, action) => {
       if (state.api.loading === "idle") {
-        state.api.loading = "pending"
+        state.api.loading = "pending";
       }
     },
-    [bestbuyapi_get_v1_products_pid_warrantiesjson_list.fulfilled]: (
-      state,
-      action
-    ) => {
+    [bestbuyapi_get_v1_products_pid_warrantiesjson_list.fulfilled]: (state, action) => {
       if (state.api.loading === "pending") {
-        state.entities = action.payload
-        state.api.loading = "idle"
+        state.entities = action.payload;
+        state.api.loading = "idle";
       }
     },
-    [bestbuyapi_get_v1_products_pid_warrantiesjson_list.rejected]: (
-      state,
-      action
-    ) => {
+    [bestbuyapi_get_v1_products_pid_warrantiesjson_list.rejected]: (state, action) => {
       if (state.api.loading === "pending") {
-        state.api.error = action.error
-        state.api.loading = "idle"
+        state.api.error = action.error;
+        state.api.loading = "idle";
       }
     }
   }
-})
+});
 export default {
   bestbuyapi_get_v1_products_pid_warrantiesjson_list,
   slice: bestbuyapi_response_get_GetProductWarrantiesSlice
-}
+};
